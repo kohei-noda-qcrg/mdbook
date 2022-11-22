4b09dc179e426eb525e3dded82708fc1183c0395
Kohei Noda <kohei-noda@hiroshima-u.ac.jp>
2022-11-22 18:30:52 +0900

    Use Next-ui

--- src/pages/markdown.tsx
+++ src/pages/markdown.tsx
@@ -4,16 +4,29 @@ import useAspidaSWR from '@aspida/swr'
 import { apiClient } from '~/utils/apiClient'
 import type { FormEvent, ChangeEvent } from 'react'
 import Layout from '~/components/Layout'
-import type { NextPage } from 'next'
+import { NextPage } from 'next'
+import { Modal, Button, Text, Input } from '@nextui-org/react'
 
 const MarkdownPage: NextPage = () => {
   const { data: markdowns, error, mutate } = useAspidaSWR(apiClient.markdowns)
   const [markdown, setMarkdown] = useState('')
+  const [title, setTitle] = useState('')
+  const [visible, setVisible] = useState(false)
   const inputMarkdown = useCallback(
     (e: ChangeEvent<HTMLInputElement>) => setMarkdown(e.target.value),
     []
   )
-  console.log(markdowns)
+  const openModal = () => setVisible(true)
+  const closeModal = () => setVisible(false)
+  const createMarkdownWithTitle = useCallback(async () => {
+    if (!title) return
+
+    await apiClient.markdowns.post({
+      body: { title, content: '# test' }
+    })
+    mutate()
+    closeModal()
+  }, [title])
 
   const createMarkdown = useCallback(
     async (e: FormEvent) => {
@@ -25,9 +38,11 @@ const MarkdownPage: NextPage = () => {
       })
       setMarkdown('')
       mutate()
+      closeModal()
     },
     [markdown]
   )
+
   if (error) return <div>failed to load</div>
 
   return (
@@ -38,15 +53,38 @@ const MarkdownPage: NextPage = () => {
 
       <Layout>
         <h1>Create a Markdown file</h1>
+        {/* Open dialog if button is clicked */}
+        <Button onClick={openModal}>Open dialog</Button>
+        <Modal closeButton blur open={visible} onClose={closeModal}>
+          <Modal.Header>
+            <Text size={18}>Create a Markdown file</Text>
+          </Modal.Header>
+          <Modal.Body>
+            <Input
+              clearable
+              bordered
+              fullWidth
+              color="success"
+              placeholder="Title"
+              size="lg"
+              onChange={(e) => setTitle(e.target.value)}
+            />
+            <Button onClick={createMarkdownWithTitle}>Create</Button>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button auto onClick={closeModal}>
+              Cancel
+            </Button>
+          </Modal.Footer>
+        </Modal>
         <input type="text" onSubmit={createMarkdown} onChange={inputMarkdown} />
         <button onClick={createMarkdown}>Submit</button>
+        {markdowns?.map((markdown) => (
+          <div key={markdown.id}>
+            <h1>{markdown.title}</h1>
+          </div>
+        ))}
       </Layout>
-      {markdowns?.map((markdown) => (
-        <div key={markdown.id}>
-          <h1>{markdown.title}</h1>
-          <p>{markdown.content}</p>
-        </div>
-      ))}
     </>
   )
 }
