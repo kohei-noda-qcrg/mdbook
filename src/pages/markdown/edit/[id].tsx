92d1058fe620cbe6260c4a032445f5963dd44486
Kohei Noda <kohei-noda@hiroshima-u.ac.jp>
2022-12-05 00:23:31 +0900

    Update Markdown Editor

--- src/pages/markdown/edit/[id].tsx
+++ src/pages/markdown/edit/[id].tsx
@@ -1,15 +1,16 @@
-import useAspidaSWR from '@aspida/swr'
-import { NextPage } from 'next'
-import Head from 'next/head'
-import { useRouter } from 'next/router'
-import Layout from '~/components/Layout'
-import { apiClient } from '~/utils/apiClient'
-import dynamic from 'next/dynamic'
-import { useEffect, useState } from 'react'
 import '@uiw/react-markdown-editor/markdown-editor.css'
 import '@uiw/react-markdown-preview/markdown.css'
+import { apiClient } from '~/utils/apiClient'
+import { Box, Button } from '@mui/material'
+import { NextPage } from 'next'
+import { useEffect, useState } from 'react'
+import { useRouter } from 'next/router'
+import dynamic from 'next/dynamic'
+import Head from 'next/head'
+import Layout from '~/components/Layout'
+import Link from 'next/link'
 import rehypeSanitize from 'rehype-sanitize'
-import { Button } from '@mui/material'
+import useAspidaSWR from '@aspida/swr'
 
 const MarkdownEditor = dynamic(() => import('@uiw/react-markdown-editor'), {
   ssr: false
@@ -25,7 +26,6 @@ const MarkdownPage: NextPage = () => {
     mutate
   } = useAspidaSWR(apiClient.markdowns._markdownId(Number(id)))
   useEffect(() => {
-    console.log(id, markdown)
     if (markdown) {
       setTitle(markdown.title)
       setContent(markdown.body)
@@ -33,24 +33,18 @@ const MarkdownPage: NextPage = () => {
   }, [markdown])
 
   const handleSave = async () => {
-    console.log('markdown', markdown, 'content', content)
     const d = await apiClient.markdowns._markdownId(Number(id)).patch({
       body: { title, body: content }
     })
-    console.log('statuscheck', d)
     if (d.status === 204) {
-      console.log('success', d)
-    } else {
-      console.log('failed', d)
+      mutate()
     }
-    mutate()
   }
 
   const [title, setTitle] = useState('')
   const [content, setContent] = useState('loading...')
   const handleChange = (content: string) => {
     setContent(content)
-    console.log('content', content)
   }
 
   return (
@@ -64,12 +58,26 @@ const MarkdownPage: NextPage = () => {
         <MarkdownEditor
           visible={true}
           value={content}
-          style={{ height: '80vh', width: '90vw' }}
+          style={{ height: '70vh', width: '90vw' }}
           // eslint-disable-next-line @typescript-eslint/no-unused-vars
           onChange={(value, _) => handleChange(value)}
           previewProps={{ rehypePlugins: [[rehypeSanitize]] }}
         />
-        <Button onClick={handleSave}>Save</Button>
+        <Box
+          style={{
+            display: 'flex',
+            justifyContent: 'space-between',
+            marginTop: '1em',
+            width: '20vw'
+          }}
+        >
+          <Button onClick={handleSave} variant="outlined">
+            Save
+          </Button>
+          <Button onClick={handleSave} variant="outlined" color="inherit">
+            <Link href="/markdown">Cancel Edit</Link>
+          </Button>
+        </Box>
       </Layout>
     </>
   )
