99e237932df089736e5f61afd29fa01aa38bb8e5
Kohei Noda <ponserviceregist@gmail.com>
2023-10-20 14:12:02 +0900

    View book

--- src/components/Book.tsx
+++ src/components/Book.tsx
@@ -1,44 +1,20 @@
-import { useState } from "react";
-
 import { type Book } from "@prisma/client";
 
 const Book = ({
   book,
-  onDelete,
+  onViewBook,
 }: {
   book: Book;
-  onDelete: () => void;
+  onViewBook: (book: Book) => void;
 }) => {
-  const [isOpen, setIsOpen] = useState(false);
-
   return (
     <>
       <div
         key={book.id}
-        className={`collapse collapse-arrow border ${
-          isOpen ? "collapse-open" : ""
-        }`}
+        className="btn flex justify-start"
+        onClick={() => onViewBook(book)}
       >
-        <div className="collapse-title" onClick={() => setIsOpen(!isOpen)} placeholder="Title">
-          {book.title}
-        </div>
-        <div className="collapse-content">
-          <div>{book.description}</div>
-          <div>{book.author ?? ""}</div>
-          <div>{book.coverImage ?? ""}</div>
-          <div>{book.content}</div>
-        </div>
-        {/* Add edit button at the right bottom of this component */}
-        <div className="card-actions mx-3 flex justify-end p-1">
-          <div className="btn btn-primary btn-sm">
-            <a href={`edit/${book.id}`}>Edit</a>
-          </div>
-          <div className="btn btn-primary btn-outline btn-sm">
-            <a href="#" onClick={onDelete}>
-              Delete
-            </a>
-          </div>
-        </div>
+        {book.title}
       </div>
     </>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,8 @@ import Head from "next/head";
 import Book from "~/components/Book";
 import { api } from "~/utils/api";
 import { useRouter } from "next/router";
+import { useState } from "react";
+import ReactMarkdown from "react-markdown";
 
 export default function Home() {
   const { data: session } = useSession();
@@ -31,29 +33,60 @@ export default function Home() {
     await router.push(`/edit/${bookId}`);
   };
 
+  const [book, setBook] = useState<Book | null>(null);
+
   return (
     <>
       <Head>
         <title>mdbook - Markdown book memo</title>
       </Head>
-      <div className="mx-2 flex justify-center">
-        {books?.map((book: Book) => (
-          <Book
-            key={book.id}
-            onDelete={() => {
-              deleteBook.mutate({ id: book.id });
+      <div className="flex">
+        <div className="flex w-1/4 flex-col gap-2">
+          {books?.map((book: Book) => (
+            <Book
+              key={book.id}
+              onViewBook={(book: Book) => {
+                setBook(book);
+              }}
+              book={book}
+            />
+          ))}
+          <div
+            className="btn btn-info btn-outline"
+            onClick={() => {
+              void handleNewBook();
             }}
-            book={book}
-          />
-        ))}
-        <div
-          className="btn"
-          onClick={() => {
-            void handleNewBook();
-          }}
-        >
-          Add new book
+          >
+            Add new book
+          </div>
         </div>
+        {book && (
+          <div className="mx-2 w-3/4">
+            <div className="flex gap-5">
+              <div className="text-4xl font-bold">{book.title}</div>
+              <div
+                className="btn btn-info btn-outline"
+                onClick={() => {
+                  void router.push(`/edit/${book.id}`);
+                }}
+              >
+                Edit
+              </div>
+              <div
+                className="btn btn-warning btn-outline"
+                onClick={() => {
+                  deleteBook.mutate({ id: book.id });
+                }}
+              >
+                Delete
+              </div>
+            </div>
+            <div className="text-2xl">{book.description}</div>
+            <div className="prose lg:prose-xl">
+              <ReactMarkdown>{book.content}</ReactMarkdown>
+            </div>
+          </div>
+        )}
       </div>
     </>
   );
